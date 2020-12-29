e2301f105df85cf764d7511f5c0d6705a166d88d
curt <curt.k@kakaomobility.com>
2020-12-29 09:17:36 +0900

    minor

--- 91_geometry_shader_houses.cpp
+++ 91_geometry_shader_houses.cpp
@@ -3,6 +3,7 @@
 
 #include <shader_s.h>
 #include <iostream>
+#include <tuple>
 
 const char *VERTEX_SHADER = R"(
 #version 330 core
@@ -48,17 +49,20 @@ void build_house(vec4 position)
     fColor = gs_in[0].color; // gs_in[0] since there's only one input vertex
     gl_Position = position + vec4(-0.2, -0.2, 0.0, 0.0); // 1:bottom-left
     EmitVertex();
+
     gl_Position = position + vec4( 0.2, -0.2, 0.0, 0.0); // 2:bottom-right
     EmitVertex();
+
     gl_Position = position + vec4(-0.2,  0.2, 0.0, 0.0); // 3:top-left
     EmitVertex();
+
     gl_Position = position + vec4( 0.2,  0.2, 0.0, 0.0); // 4:top-right
     EmitVertex();
-    gl_Position = position + vec4( 0.0,  0.4, 0.0, 0.0); // 5:top
 
-    // give their roofs a little snow by giving the last vertex a color of its own
-    fColor = vec3(1.0, 1.0, 1.0);
+    gl_Position = position + vec4( 0.0,  0.4, 0.0, 0.0); // 5:top
+    fColor = vec3(1.0, 1.0, 1.0);   // give their roofs a little snow by giving the last vertex a color of its own
     EmitVertex();
+
     EndPrimitive();
 }
 
@@ -68,11 +72,45 @@ void main() {
 )";
 
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
+
+
+auto make_model() {
+  static float points[] = {
+    -0.5f,  0.5f,   1.0f, 0.0f, 0.0f, // top-left(red)
+    0.5f,  0.5f,    0.0f, 1.0f, 0.0f, // top-right(green)
+    0.5f, -0.5f,    0.0f, 0.0f, 1.0f, // bottom-right(blue)
+    -0.5f, -0.5f,   1.0f, 1.0f, 0.0f  // bottom-left(yellow)
+  };
+
+  unsigned int VBO, VAO;
+  glGenBuffers(1, &VBO);
+  glGenVertexArrays(1, &VAO);
+
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(points), &points, GL_STATIC_DRAW);
+
+  // pos
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+
+  // color
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+
+  glBindVertexArray(0);
+  return std::make_tuple(VAO, VBO);
+}
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
 
 int main()
 {
@@ -98,66 +136,30 @@ int main()
   }
   glfwMakeContextCurrent(window);
 
-  // glad: load all OpenGL function pointers
-  // ---------------------------------------
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
   {
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
   }
 
-  // configure global opengl state
-  // -----------------------------
+  Shader shader(VERTEX_SHADER, FRAGMENT_SHADER, GEOMETRY_SHADER);
+  auto [VAO, VBO] = make_model();
+
   glEnable(GL_DEPTH_TEST);
 
-  // build and compile shaders
-  // -------------------------
-  Shader shader(VERTEX_SHADER, FRAGMENT_SHADER, GEOMETRY_SHADER);
-  //Shader shader("9.1.geometry_shader.vs", "9.1.geometry_shader.fs", "9.1.geometry_shader.gs");
-
-  // set up vertex data (and buffer(s)) and configure vertex attributes
-  // ------------------------------------------------------------------
-  float points[] = {
-      -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // top-left
-      0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // top-right
-      0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // bottom-right
-      -0.5f, -0.5f, 1.0f, 1.0f, 0.0f  // bottom-left
-  };
-
-  unsigned int VBO, VAO;
-  glGenBuffers(1, &VBO);
-  glGenVertexArrays(1, &VAO);
-  glBindVertexArray(VAO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(points), &points, GL_STATIC_DRAW);
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
-  glEnableVertexAttribArray(1);
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
-  glBindVertexArray(0);
-
-  // render loop
-  // -----------
   while (!glfwWindowShouldClose(window))
   {
-    // render
-    // ------
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // draw points
     shader.use();
     glBindVertexArray(VAO);
     glDrawArrays(GL_POINTS, 0, 4);
 
-    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-    // -------------------------------------------------------------------------------
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
-  // optional: de-allocate all resources once they've outlived their purpose:
-  // ------------------------------------------------------------------------
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
 
@@ -165,11 +167,3 @@ int main()
   return 0;
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-  // make sure the viewport matches the new window dimensions; note that width and
-  // height will be significantly larger than specified on retina displays.
-  glViewport(0, 0, width, height);
-}
